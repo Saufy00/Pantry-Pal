b4dde31613bc45482b20db454c18ad064dfe566e
Saiful <saifulghazi98@gmail.com>
2026-07-03 10:18:36 +0800

    feat(pwa): Register Service Worker to enable native mobile installation

--- pantry/src/main.tsx
+++ pantry/src/main.tsx
@@ -2,6 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Refresh to update?")) {
+      updateSW(true);
+    }
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
